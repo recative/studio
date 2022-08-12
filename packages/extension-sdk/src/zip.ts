9f58b1fcb22a256a34feb383137463f8a901abc0
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-08-13 04:36:20 +0800

    refactor: Remove archiver since it's a mess

--- packages/extension-sdk/src/zip.ts
+++ packages/extension-sdk/src/zip.ts
@@ -1,134 +1,209 @@
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-syntax */
-import StreamZip from 'node-stream-zip';
+import log from 'electron-log';
+// @ts-ignore: It's okay
+import ZipWriter from 'zip-stream';
+import ZipReader from 'node-stream-zip';
 
 import { glob } from 'glob';
-import { join } from 'path';
-import { readFile } from 'fs/promises';
-import { createReadStream, createWriteStream } from 'fs';
-import type { QueueObject } from 'async';
-
-import originalArchiver from 'archiver';
+import { Buffer } from 'buffer';
+import { promisify } from 'util';
+import { join, basename } from 'path';
+import { readFile, lstat } from 'fs/promises';
+import { createWriteStream } from 'fs';
+import { Transform, Stream } from 'stream';
 
 import type { WriteStream } from 'fs';
+import type { ZlibOptions } from 'zlib';
+
+interface IZipData {
+  name: string;
+  comment: string;
+  date: string | Date;
+  mode: number;
+  namePrependSlash: boolean;
+  store: boolean;
+  type: 'directory' | 'file';
+}
 
 export interface IPathListItem {
-  from: string | ReadableStream | Promise<string | ReadableStream>;
+  from: string | Stream | Buffer | Promise<string | Stream | Buffer>;
   to: string;
+}
+
+export interface IZipOption {
+  comment: string;
+  forceLocalTime: boolean;
+  forceZip64: boolean;
+  store: boolean;
+  zlib: ZlibOptions;
 }
 
 export class Zip {
   protected outputStream: WriteStream;
 
   // We use an internal member here.
-  protected archive: originalArchiver.Archiver & {
-    _queue: QueueObject<unknown>;
-  };
+  protected archive: {
+    entry: (
+      source: Buffer | Stream | string,
+      data: Partial<IZipData>,
+      callback: (error: Error, entry: IZipData) => void
+    ) => void;
+    finalize: () => void;
+    getBytesWritten: () => number;
+  } & Transform;
 
   protected finished: Promise<null>;
 
+  entry: (
+    source: Buffer | Stream | string,
+    data: Partial<IZipData>
+  ) => Promise<IZipData>;
+
+  writing = false;
+
   constructor(
     public readonly filePath: string,
-    options: originalArchiver.ArchiverOptions | undefined = {
+    options: Partial<IZipOption> | undefined = {
       zlib: { level: 9 },
     }
   ) {
     this.outputStream = createWriteStream(filePath);
-    this.archive = originalArchiver(
-      'zip',
-      options
-    ) as unknown as typeof this.archive;
+    this.outputStream.close = () => {
+      log.warn('AVOID THIS');
+    };
+
+    this.archive = new ZipWriter(options) as unknown as typeof this.archive;
+    this.entry = promisify(this.archive.entry).bind(this.archive);
+
+    this.archive.pipe(this.outputStream);
 
     this.finished = new Promise((resolve) => {
+      this.archive.on('finish', () => {
+        log.error(`:: [${basename(filePath)}] Archive finish`);
+      });
+
+      this.archive.on('end', () => {
+        log.log(`:: [${basename(filePath)}] Archive end`);
+      });
+
       this.outputStream.on('finish', () => {
-        this.outputStream.close();
+        log.log(`:: [${basename(filePath)}] Stream finish`);
+        this.outputStream.end();
       });
 
       this.outputStream.on('close', () => {
+        log.log(
+          `:: [${basename(filePath)}] Stream close`,
+          this.outputStream.destroyed
+        );
         resolve(null);
       });
 
-      this.archive.pipe(this.outputStream);
+      this.archive.on('error', (error) => {
+        log.error(`:: [${basename(filePath)}] Archive error`, error);
+      });
+
+      this.archive.on('close', () => {
+        log.error(`:: [${basename(filePath)}] Archive close`);
+      });
     });
   }
 
-  waitForDrain = <T>(x: T) => {
-    return new Promise<T>((resolve, reject) => {
-      this.archive.once('error', (err) => {
-        reject(err);
-      });
+  appendText = async (text: string, to: string) => {
+    if (this.writing) {
+      throw new Error('Cannot write new content to zip while writing');
+    }
 
-      this.outputStream.once('end', () => {
-        resolve(x);
-      });
+    this.writing = true;
+    const result = await this.entry(text, { name: to });
+    this.writing = false;
 
-      this.archive._queue.drain(() => resolve(x));
+    return result;
+  };
+
+  appendFile = async (from: string | Buffer | Stream, to: string) => {
+    if (this.writing) {
+      throw new Error('Cannot write new content to zip while writing');
+    }
+
+    const writeEntry = async (source: Buffer | Stream) => {
+      this.writing = true;
+      const result = await this.entry(source, { name: to, type: 'file' });
+      this.writing = false;
+
+      return result;
+    };
+
+    if (from instanceof Buffer || from instanceof Stream) {
+      return writeEntry(from);
+    }
+
+    const buffer = await readFile(from);
+    return writeEntry(buffer);
+  };
+
+  appendFileList = async (pathListItem: IPathListItem[]) => {
+    const resolvedPathListItem = await Promise.all(
+      pathListItem.map(async (item) => {
+        const from = await item.from;
+        return { ...item, from };
+      })
+    );
+
+    // let i = 0;
+    for (const { from, to } of resolvedPathListItem) {
+      // log.log(
+      //   `:: [${basename(this.filePath)}] [${i + 1} / ${
+      //     resolvedPathListItem.length
+      //   }]`
+      // );
+
+      const resolvedFrom = from;
+      // eslint-disable-next-line no-await-in-loop
+      await this.appendFile(resolvedFrom, to);
+      // i += 1;
+    }
+
+    return pathListItem.length;
+  };
+
+  appendGlob = async (pattern: string, cwd?: string, to?: string) => {
+    const match = glob.sync(pattern, {
+      cwd,
+      dot: true,
+      stat: true,
     });
+
+    const statedMatch = (
+      await Promise.all(
+        match.map(async (relativePath) => {
+          const fullPath = join(cwd || '', relativePath);
+          const stat = await lstat(fullPath);
+
+          return { fullPath, relativePath, stat };
+        })
+      )
+    ).filter((item) => item.stat.isFile());
+
+    // let i = 0;
+    for (const { fullPath, relativePath } of statedMatch) {
+      // log.log(
+      //   `:: [${basename(this.filePath)}] [${i + 1} / ${
+      //     match.length
+      //   }] ${relativePath}`
+      // );
+
+      const targetPath = join(to || '/', relativePath);
+      // eslint-disable-next-line no-await-in-loop
+      await this.appendFile(fullPath, targetPath);
+      // i += 1;
+    }
+
+    return match.length;
   };
 
-  appendFile = (from: string | Buffer, to: string) => {
-    const drainPromise = this.waitForDrain(null);
-    this.archive.append(from, { name: to });
-
-    return drainPromise;
-  };
-
-  appendFileList = (pathListItem: IPathListItem[]) => {
-    const promise = new Promise<void>((resolve, reject) => {
-      const allTasks: Record<string, boolean> = {};
-
-      pathListItem.forEach((item) => {
-        allTasks[item.to] = false;
-      });
-
-      const handleError = (err: Error) => {
-        reject(err);
-      };
-
-      const handleEntry = (entry: originalArchiver.EntryData) => {
-        if (entry.name in allTasks) {
-          allTasks[entry.name] = true;
-        }
-
-        if (Object.values(allTasks).every((x) => x)) {
-          this.archive.off('error', handleError);
-          this.archive.off('entry', handleEntry);
-          resolve();
-        }
-      };
-
-      this.archive.once('error', handleError);
-      this.archive.on('entry', handleEntry);
-
-      pathListItem.map(async ({ from, to }) => {
-        const resolvedFrom = await from;
-        this.archive.append(
-          // @ts-ignore - `readStream` is a readable stream.
-          typeof resolvedFrom === 'string' ? createReadStream(from) : from,
-          {
-            name: to,
-          }
-        );
-      });
-    });
-
-    return promise;
-  };
-
-  appendDir = (from: string, to: string) => {
-    const drainPromise = this.waitForDrain(null);
-    this.archive.directory(from, to);
-
-    return drainPromise;
-  };
-
-  appendGlob = (globStr: string, cwd?: string) => {
-    const files = glob.sync(globStr, { cwd });
-
-    const drainPromise = this.waitForDrain(files);
-    this.archive.glob(globStr, { cwd });
-
-    return drainPromise;
+  appendDir = async (from: string, to: string) => {
+    return this.appendGlob('**/*', from, to);
   };
 
   transfer = async (
@@ -137,7 +212,7 @@ export class Zip {
     to: string | null = null,
     exclude: string[] | null = null
   ) => {
-    const baseZip = new StreamZip.async({ file: fromZip });
+    const baseZip = new ZipReader.async({ file: fromZip });
     const baseZipFileList = Object.entries(await baseZip.entries())
       .map(([key, entry]) => {
         if (!entry.isFile) {
@@ -163,21 +238,42 @@ export class Zip {
         const pathBase = to ? join(to, entryName) : entryName;
 
         return {
-          from: (await baseZip.stream(
-            entryName
-          )) as unknown as ReadableStream<unknown>,
+          from: await baseZip.stream(entryName),
           to: from ? pathBase.replace(from, '') : pathBase,
         };
       });
 
-    return this.appendFileList(await Promise.all(baseZipFileList));
+    const allFiles = await Promise.all(baseZipFileList);
+
+    log.log(
+      `:: [${basename(this.filePath)}] Transfer ${allFiles.length} files...`
+    );
+
+    if (!allFiles.length) {
+      return null;
+    }
+
+    return this.appendFileList(allFiles);
   };
 
   done = async () => {
-    this.archive.append('yay', { name: '.recative' });
+    if (this.writing) {
+      throw new Error('Writing is not finished, unable to close the file');
+    }
+
+    log.log(`:: [${basename(this.filePath)}] Closing archive`);
+
+    // await this.appendText('yay', '.recative');
+
     this.archive.finalize();
 
     await this.finished;
+
+    log.log(
+      `:: [${basename(
+        this.filePath
+      )}] Written ${this.archive.getBytesWritten()} bytes`
+    );
 
     return readFile(this.filePath);
   };
