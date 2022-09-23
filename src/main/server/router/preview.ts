06e53962acdccb8c589ba299c07bceca2e31422a
Ben Pig Chu <benpichu@gmail.com>
2022-09-23 18:01:13 +0800

    fix: Wrong episodeId in getEnvVariableHandler

--- src/main/server/router/preview.ts
+++ src/main/server/router/preview.ts
@@ -8,7 +8,5 @@ export const getEnvVariableHandler = async (
 ) => {
   const query = request.query as { apHost?: string; episodeId?: string };
 
-  reply
-    .status(200)
-    .send(await getEnvVariable(query.apHost || request.hostname));
+  reply.status(200).send(await getEnvVariable(query.episodeId));
 };
