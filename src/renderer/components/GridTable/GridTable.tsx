5a005bcd4fd1da301c0af8d3f7cb22f3dc6080f9
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-12-21 10:51:59 +0800

    refactor: Extract general purpose table component

---
+++ src/renderer/components/GridTable/GridTable.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import cn from 'classnames';
+
+import { useStyletron } from 'baseui';
+import { StyledTable, StyledHeadCell, StyledBodyCell } from 'baseui/table-grid';
+
+import { EmptySpace } from 'components/EmptyState/EmptyState';
+import { RecativeBlock } from 'components/Block/RecativeBlock';
+
+export interface IColumnConfig<K extends string> {
+  id: K;
+  order: number;
+  width: string;
+  content: React.ReactNode;
+}
+
+export type IData<K extends string> = {
+  id: string;
+} & {
+  [key in K]: React.ReactNode;
+};
+
+export interface IGridTableProps<K extends string> {
+  columns: IColumnConfig<K>[];
+  data?: IData<K>[];
+  emptyHeader: string;
+  emptyContent: string;
+  Actions?: React.FC<IData<K>>;
+}
+
+const tableStyle = {
+  overflowX: 'initial',
+  overflowY: 'initial',
+} as const;
+
+const headerStyle = {
+  top: '300px',
+  display: 'contents',
+  position: 'sticky',
+} as const;
+
+const headerCellStyle = {
+  height: '20px',
+  textTransform: 'capitalize',
+} as const;
+
+const bodyStyle = {
+  display: 'contents',
+} as const;
+
+const DEFAULT_ACTIONS = () => <></>;
+
+export const GridTable = <K extends string>({
+  data,
+  columns,
+  emptyHeader,
+  emptyContent,
+  Actions = DEFAULT_ACTIONS,
+}: IGridTableProps<K>) => {
+  const [css, theme] = useStyletron();
+
+  const cellStyle = React.useMemo(
+    () =>
+      css({
+        height: '40px',
+        borderBottomColor: theme.colors.borderTransparent || 'black',
+        borderBottomWidth: '1px',
+        borderBottomStyle: 'solid',
+        lineHeight: '40px !important',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      } as const),
+    [css, theme.colors.borderTransparent]
+  );
+
+  const sortedColumns = React.useMemo(
+    () => columns.sort((a, b) => a.order - b.order),
+    [columns]
+  );
+
+  const gridTemplateColumns = React.useMemo(
+    () => `${sortedColumns.map((x) => x.width).join(' ')} max-content`,
+    [sortedColumns]
+  );
+
+  const gridTemplateRowStyles = React.useMemo(
+    () =>
+      css({
+        gridTemplateRows: `repeat(${data?.length ?? -1 + 1}, min-content)`,
+      }),
+    [css, data?.length]
+  );
+
+  return (
+    <StyledTable
+      role="grid"
+      className={cn(css(tableStyle), gridTemplateRowStyles)}
+      $gridTemplateColumns={gridTemplateColumns}
+    >
+      <RecativeBlock className={css(headerStyle)} role="row">
+        {sortedColumns.map((x) => (
+          <StyledHeadCell key={x.id} className={css(headerCellStyle)}>
+            {x.content}
+          </StyledHeadCell>
+        ))}
+        <StyledHeadCell className={headerCellStyle} />
+      </RecativeBlock>
+      {data?.map((row) => {
+        return (
+          <RecativeBlock key={row.id} className={css(bodyStyle)} role="row">
+            {sortedColumns.map((column) => (
+              <StyledBodyCell
+                key={`${column.id}-${row.id}`}
+                className={cellStyle}
+              >
+                {row[column.id]}
+              </StyledBodyCell>
+            ))}
+
+            {Actions && (
+              <StyledBodyCell className={cellStyle}>
+                <Actions {...row} />
+              </StyledBodyCell>
+            )}
+          </RecativeBlock>
+        );
+      })}
+      {!data?.length && (
+        <RecativeBlock gridColumn={`1 / ${sortedColumns.length + 1}`}>
+          <EmptySpace title={emptyHeader} content={emptyContent} />
+        </RecativeBlock>
+      )}
+    </StyledTable>
+  );
+};
--- src/renderer/pages/User/components/PermissionList.tsx
+++ src/renderer/pages/User/components/PermissionList.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import cn from 'classnames';
 
 import { useAsync } from '@react-hookz/web';
 
-import { useStyletron } from 'baseui';
-import { StyledTable, StyledHeadCell, StyledBodyCell } from 'baseui/table-grid';
-
-import { EmptySpace } from 'components/EmptyState/EmptyState';
-import { RecativeBlock } from 'components/Block/RecativeBlock';
-
 import { server } from 'utils/rpc';
+import { GridTable, IColumnConfig } from 'components/GridTable/GridTable';
 
 export interface IPermissionListActionsProps {
   id: string;
@@ -19,28 +13,26 @@ export interface IPermissionListProps {
   Actions?: React.FC<IPermissionListActionsProps>;
 }
 
-const tableStyle = {
-  overflowX: 'initial',
-  overflowY: 'initial',
-} as const;
+const permissionKeys = ['index', 'notes'] as const;
 
-const headerStyle = {
-  top: '300px',
-  display: 'contents',
-  position: 'sticky',
-} as const;
+const columnConfigs: IColumnConfig<PermissionKeys>[] = [
+  {
+    id: 'index',
+    order: 0,
+    width: 'max-content',
+    content: 'ID',
+  },
+  {
+    id: 'notes',
+    order: 1,
+    width: 'auto',
+    content: 'Notes',
+  },
+];
 
-const bodyStyle = {
-  display: 'contents',
-} as const;
+type PermissionKeys = typeof permissionKeys[number];
 
-const DEFAULT_ACTIONS = () => <></>;
-
-export const PermissionList: React.FC<IPermissionListProps> = ({
-  Actions = DEFAULT_ACTIONS,
-}) => {
-  const [css, theme] = useStyletron();
-
+export const PermissionList: React.FC<IPermissionListProps> = ({ Actions }) => {
   const [permissions, permissionsActions] = useAsync(() => {
     return server.getPermissions();
   });
@@ -49,80 +41,23 @@ export const PermissionList: React.FC<IPermissionListProps> = ({
     permissionsActions.execute();
   }, [permissionsActions]);
 
-  const cellStyle = React.useMemo(
+  const data = React.useMemo(
     () =>
-      css({
-        height: '40px',
-        borderBottomColor: theme.colors.borderTransparent || 'black',
-        borderBottomWidth: '1px',
-        borderBottomStyle: 'solid',
-        lineHeight: '40px !important',
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-      } as const),
-    [css, theme.colors.borderTransparent]
-  );
-
-  const gridHeaderStyle = React.useMemo(
-    () =>
-      css({
-        height: '20px',
-        textTransform: 'capitalize',
-      }),
-    [css]
-  );
-
-  const gridTemplateRowStyles = React.useMemo(
-    () =>
-      css({
-        gridTemplateRows: `repeat(${
-          permissions.result?.length ?? -1 + 1
-        }, min-content)`,
-      }),
-    [css, permissions.result?.length]
+      permissions.result?.map((permission) => ({
+        id: permission.id,
+        index: permission.id,
+        notes: permission.comment,
+      })),
+    [permissions.result]
   );
 
   return (
-    <StyledTable
-      role="grid"
-      className={cn(css(tableStyle), gridTemplateRowStyles)}
-      $gridTemplateColumns="max-content auto max-content"
-    >
-      <RecativeBlock id="checker" className={css(headerStyle)} role="row">
-        <StyledHeadCell className={gridHeaderStyle}>ID</StyledHeadCell>
-        <StyledHeadCell className={gridHeaderStyle}>Notes</StyledHeadCell>
-        <StyledHeadCell className={gridHeaderStyle}></StyledHeadCell>
-      </RecativeBlock>
-      {permissions.result?.map((permission) => {
-        return (
-          <RecativeBlock
-            key={permission.id}
-            className={css(bodyStyle)}
-            role="row"
-          >
-            <StyledBodyCell className={cellStyle}>
-              <RecativeBlock fontWeight={500}>{permission.id}</RecativeBlock>
-            </StyledBodyCell>
-            <StyledBodyCell className={cellStyle}>
-              {permission.comment}
-            </StyledBodyCell>
-            {Actions && (
-              <StyledBodyCell className={cellStyle}>
-                <Actions id={permission.id} />
-              </StyledBodyCell>
-            )}
-          </RecativeBlock>
-        );
-      })}
-      {!permissions.result?.length && (
-        <RecativeBlock gridColumn="1 / 4">
-          <EmptySpace
-            title="No permission"
-            content="Creating new permission entries by synchronizing the episode list or manually adding one."
-          />
-        </RecativeBlock>
-      )}
-    </StyledTable>
+    <GridTable
+      columns={columnConfigs}
+      Actions={Actions}
+      data={data}
+      emptyHeader="No permission"
+      emptyContent="Creating new permission entries by synchronizing the episode list or manually adding one."
+    />
   );
 };
